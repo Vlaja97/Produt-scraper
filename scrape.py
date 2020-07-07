6ef0455d5f07cba1ab47d658d92b7c9cb579b262
Vlaja97 <v.vlajic.vlada@gmail.com>
2020-07-07 16:24:54 +0200

    added link to the products

--- scrape.py
+++ scrape.py
@@ -16,6 +16,11 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 name = []
 price = []
+product_link = []
+for link in soup.find_all('a', class_='product-image'):
+    if link.has_attr('href'):
+        product_link.append(link['href'])
+
 for card in soup.find_all('div', class_='product-info'):
     name_tmp = card.find('h2', class_='product-name').text
     name.append(name_tmp)
@@ -24,12 +29,13 @@ for card in soup.find_all('div', class_='product-info'):
 
 
 ### Format data
-df = pd.DataFrame(zip(name, price), columns=['Name', 'Price'])
+df = pd.DataFrame(zip(name, price, product_link), columns=['Name', 'Price', 'Link'])
 df['Price'] = (df['Price'].str.replace('RSD', '').str.strip())
 # Formating price to show more digits
 ### TODO set prices to float
 #df['Price'] = df['Price'].astype(float)
 #pd.set_option('display.float_format','{:.3f}'.format)
+
 
 
 # Create DB
@@ -43,6 +49,7 @@ class Product(Base):
 
     name = Column(String, primary_key=True)
     price = Column(Float)
+    product_link = Column(String)
     def __repr__(self):
         return f'Product {self.name} - Price {self.price}'
     
