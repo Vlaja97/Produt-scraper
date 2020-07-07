e323f9d2155b2d960ddb5493eece71b2590fec8b
Vlaja97 <v.vlajic.vlada@gmail.com>
2020-07-07 17:28:58 +0200

    scraping acros multiple pages

--- scrape.py
+++ scrape.py
@@ -10,23 +10,33 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 ### Scrape data
-url = 'https://www.emmi.rs/racunari-komponente/racunarske-komponente/graficke-kartice-vga.html'
-page = requests.get(url,headers={"User-Agent":"Mozilla/5.0"})
-soup = BeautifulSoup(page.content, 'html.parser')
 
 name = []
 price = []
 product_link = []
-for link in soup.find_all('a', class_='product-image'):
-    if link.has_attr('href'):
-        product_link.append(link['href'])
+current_page = 0
+url = 'https://www.emmi.rs/racunari-komponente/racunarske-komponente/graficke-kartice-vga.html'
+while True:
+    page = requests.get(url,headers={"User-Agent":"Mozilla/5.0"})
+    soup = BeautifulSoup(page.content, 'html.parser')
+    print(url)
 
-for card in soup.find_all('div', class_='product-info'):
-    name_tmp = card.find('h2', class_='product-name').text
-    name.append(name_tmp)
-    price_tmp = card.find('span', class_='price').text
-    price.append(price_tmp)
+    for link in soup.find_all('a', class_='product-image'):
+        if link.has_attr('href'):
+            product_link.append(link['href'])
 
+    for card in soup.find_all('div', class_='product-info'):
+        name_tmp = card.find('h2', class_='product-name').text
+        name.append(name_tmp)
+        price_tmp = card.find('span', class_='price').text
+        price.append(price_tmp)
+    # Finding link for next page if it exists
+    next_page = soup.find('a', class_='next i i-arrow-right')
+    if next_page:
+        url = next_page.get('href')
+        current_page += 1
+    else:
+        break
 
 ### Format data
 df = pd.DataFrame(zip(name, price, product_link), columns=['Name', 'Price', 'Link'])
@@ -35,7 +45,6 @@ df['Price'] = (df['Price'].str.replace('RSD', '').str.strip())
 ### TODO set prices to float
 #df['Price'] = df['Price'].astype(float)
 #pd.set_option('display.float_format','{:.3f}'.format)
-
 
 
 # Create DB
